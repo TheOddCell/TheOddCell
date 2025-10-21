a0d886fa62cc4917fe39f71c79d60a327c8351c9
/home/odd <me@oddcell.ca>
2025-10-20 20:53:44 -0400

    Center align identicon output in odd.sh

--- odd.sh
+++ odd.sh
@@ -4,4 +4,4 @@
 # - identicon
 # - imagemagick
 # - kitten (plus an terminal emulator that supports the kitty image protocol)
-printf "odd" | identicon | magick png:- -fuzz 0% -fill "#0080FF" -opaque "#77DC6A" -fuzz 0% -transparent "#F0F0F0" png:- | kitten icat --align=left
+printf "odd" | identicon | magick png:- -fuzz 0% -fill "#0080FF" -opaque "#77DC6A" -fuzz 0% -transparent "#F0F0F0" png:- | kitten icat --align=center --use-window-size=10,10,100,100
